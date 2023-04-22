4134b1a3114c6c911e3731fed7dcf2bf67745f80
Odogwu <becomingadev@gmail.com>
2023-04-22 17:45:46 +0100

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div>Yesed!</div>
+    <div>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/forgotpassword' element={<ForgotPassword />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
